ddf5f8bf000dd2ecf0b637cab2b1654f7cda7af3
WilhamsGomes <wilhamsgoomes@gmail.com>
2022-11-16 23:03:58 -0300

    Subindo projeto

---
+++ src/components/Tarefas/BaseTarefa.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <v-list-item 
+          :class="{'light-blue lighten-5 text-decoration-line-through':tarefa.concluido}" 
+          @click="changeValue()"
+        >
+              <template>
+                <v-list-item-action>
+                  <v-checkbox :input-value="tarefa.concluido"></v-checkbox>
+                </v-list-item-action>
+
+                <v-list-item-content>
+                  <v-list-item-title>{{tarefa.titulo}}</v-list-item-title>
+                </v-list-item-content>
+              </template>
+        </v-list-item>  
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"BaseTarefa",
+        props:['tarefa'],
+        data(){
+          return{
+            tarefaR: this.tarefa
+          }
+        },
+
+        methods: {
+
+          changeValue(){
+            this.tarefaR.concluido = !this.tarefaR.concluido
+          }
+
+        } 
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/views/TarefasView.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <v-col cols="12">
+            <v-text-field
+              v-model="campoInput"
+              label="Qual sua tarefa?"
+              outlined
+              clearable
+              @keyup.enter="handleAddTarefa()"
+            >
+            </v-text-field>
+        </v-col>
+
+        <v-list flat subheader >
+            <v-list-item-group
+              multiple
+              active-class=""
+            >
+            <div
+              v-for="(tarefa, index) in tarefas" :key="index"
+            >
+              <BaseTarefa
+                :tarefa="tarefa"
+              />        
+            </div>
+            </v-list-item-group>
+        </v-list>
+    </div>
+</template>
+
+<script>
+  import BaseTarefa from '@/components/Tarefas/BaseTarefa.vue';
+
+  export default {
+    name: "HomeView",
+    components: { 
+      BaseTarefa 
+    },
+    data(){
+      return{
+        campoInput:null,
+        tarefas: [
+          {titulo: "Ir ao mercado", concluido: false},
+          {titulo: "Assistir aula", concluido: true},
+          {titulo: "Farmácia", concluido: true},
+        ],
+      }
+    },
+      methods:{
+        handleAddTarefa(){
+            if(this.campoInput){
+              this.tarefas.push({
+                titulo:this.campoInput,
+                concluido: false
+              })
+              this.campoInput = null
+            }
+        }
+    },
+}
+</script>
